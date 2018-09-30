d5ff4cd7d3b2c70be9c1e65bbc987da1d939ae2d
Georges <Georges@192.168.0.145>
2018-09-29 23:21:12 -0300

    a

--- src/Assets/Board.java
+++ src/Assets/Board.java
@@ -2,5 +2,29 @@ package Assets;
 
 public class Board {
 
-    int i = 98;
+	//-------------------//
+	// Instance variable //
+	//-------------------//
+	
+	private static final int DEFAULT_POINTS = 12;
+    private int points;
+    private int side;
+    
+  
+    //--------------//
+    // Constructors //
+    //--------------//
+    
+    public Board() {				//default constructor
+    	points = DEFAULT_POINTS;
+    }
+    
+    public Board(int points) {
+    	this.points = points;
+    }
+    
+    public int getPoints() {
+    	return points;
+    }
+    
 }
--- src/Game/Player.java
+++ src/Game/Player.java
@@ -2,4 +2,25 @@ package Game;
 
 public class Player {
 
+	//-------------------//
+	// Instance variable //
+	//-------------------//
+	
+	private int numShips;
+	private int numSankShips = 0;
+	private int numHits = 0;
+	private int numMiss = 0;
+	
+	//--------------//
+	// Constructors //
+	//--------------//
+	
+	public Player() {}
+	
+	//---------//
+	// Methods //
+	//---------//
+	
+	public 
+
 }
